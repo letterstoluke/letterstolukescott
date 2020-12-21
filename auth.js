380632a2fa25b2c18770b1e1d3bba4b5f87713ed
letterstoluke <68086721+letterstoluke@users.noreply.github.com>
2020-12-21 09:08:43 -0800

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,18 @@
+// Initialize Firebase
+firebase.initializeApp(fbConfig);
+var DB = firebase.database();
+
+// Auth
+var uid = "";
+// Using a popup.
+var provider = new firebase.auth.GoogleAuthProvider();
+// provider.addScope('profile');
+// provider.addScope('email');
+firebase.auth().signInWithPopup(provider).then(function (result) {
+  // This gives you a Google Access Token.
+  var token = result.credential.accessToken;
+  console.log(token);
+  // The signed-in user info.
+  var user = result.user;
+  uid = user.uid;
+  // consol
